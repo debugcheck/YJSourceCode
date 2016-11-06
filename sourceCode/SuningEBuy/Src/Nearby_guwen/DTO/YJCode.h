f0eaeaaae3ffacfee948a783d0aa620d7565d4ae
YJHou <yjhou@YJHouMacBookPro.local>
2016-11-06 18:20:42 +0800

    add model

---
+++ sourceCode/SuningEBuy/Src/Nearby_guwen/DTO/YJCode.h
@@ -0,0 +1,13 @@
+//
+//  YJCode.h
+//  SuningEBuy
+//
+//  Created by YJHou on 2016/11/6.
+//  Copyright © 2016年 Suning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YJCode : NSObject
+
+@end
